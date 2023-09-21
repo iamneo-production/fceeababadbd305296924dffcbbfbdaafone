859f6c7c25464b5bc3182af08ba8d940e8db004b
Iamneo <techadmin@iamneo.ai>
2023-09-21 11:22:58 +0000

    Backup Commit

---
+++ Project/src/test/java/uistore/HomepageUI.java
@@ -0,0 +1,11 @@
+package uistore;
+
+import org.openqa.selenium.By;
+
+public class HomepageUI {
+    public static By username = By.name("username");
+    public static By password = By.id("password");
+    // public static By submit = By.id("submit");
+    public static By submit = By.xpath("//*[@id=\"submit\"]")
+   
+}
